fe5dc482ba985d125e806df9caf9a745194c71ea
Rob Cottrill <rcottrill@ACAC0538.ipt.aol.com>
2018-10-24 17:31:33 +0100

    Changes

--- DSC/CreateADForest/CreateADForest.ps1
+++ DSC/CreateADForest/CreateADForest.ps1
@@ -1,7 +1,7 @@
-﻿configuration CreateADForest 
-{ 
-   param 
-   ( 
+﻿configuration CreateADForest
+{
+   param
+   (
         [Parameter(Mandatory)]
         [String]$DomainName,
 
@@ -10,8 +10,8 @@
 
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
-    ) 
-    
+    )
+
     Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
     $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
@@ -19,49 +19,62 @@
 
     Node localhost
     {
-        LocalConfigurationManager 
+        LocalConfigurationManager
         {
             RebootNodeIfNeeded = $true
         }
 
-	    WindowsFeature DNS 
-        { 
-            Ensure = "Present" 
-            Name = "DNS"		
+        WindowsFeature DNS
+        {
+            Ensure = "Present"
+            Name = "DNS"
         }
 
         Script EnableDNSDiags
-	    {
-      	    SetScript = { 
-		        Set-DnsServerDiagnostics -All $true
-                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
+        {
+      	    SetScript = {
+                Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics"
             }
             GetScript =  { @{} }
             TestScript = { $false }
-	        DependsOn = "[WindowsFeature]DNS"
+            DependsOn = "[WindowsFeature]DNS"
         }
 
-	    WindowsFeature DnsTools
-	    {
-	        Ensure = "Present"
+        WindowsFeature DnsTools
+        {
+            Ensure = "Present"
             Name = "RSAT-DNS-Server"
             DependsOn = "[WindowsFeature]DNS"
-	    }
-
-        xDnsServerAddress DnsServerAddress 
-        { 
-            Address        = '127.0.0.1' 
-            InterfaceAlias = $InterfaceAlias
-            AddressFamily  = 'IPv4'
-	        DependsOn = "[WindowsFeature]DNS"
         }
 
-        WindowsFeature ADDSInstall 
-        { 
-            Ensure = "Present" 
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = '127.0.0.1'
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk {
+            DiskNumber = 2
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
             Name = "AD-Domain-Services"
-	        DependsOn="[WindowsFeature]DNS" 
-        } 
+            DependsOn="[WindowsFeature]DNS"
+        }
 
         WindowsFeature ADDSTools
         {
@@ -74,19 +87,24 @@
         {
             Ensure = "Present"
             Name = "RSAT-AD-AdminCenter"
-            DependsOn = "[WindowsFeature]ADDSInstall"
+            DependsOn = "[WindowsFeature]ADDSTools"
         }
-         
-        xADDomain FirstDS 
+
+        xADDomain FirstDS
         {
             DomainName = $DomainName
             DomainAdministratorCredential = $DomainCreds
             SafemodeAdministratorPassword = $DomainCreds
-            DatabasePath = "C:\Windows\NTDS"
-            LogPath = "C:\Windows\NTDS"
-            SysvolPath = "C:\Windows\SYSVOL"
-	        DependsOn = "[WindowsFeature]ADDSInstall"
-        } 
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+            DependsOn = @("[WindowsFeature]ADDSInstall", "[xDisk]ADDataDisk")
+        }
+
+        xPendingReboot RebootAfterPromotion{
+            Name = "RebootAfterPromotion"
+            DependsOn = "[xADDomain]FirstDS"
+        }
 
    }
-} 
+}
